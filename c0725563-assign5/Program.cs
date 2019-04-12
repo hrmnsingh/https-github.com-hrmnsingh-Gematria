8e445cfa4d352b954ec7a4dd09f977e9c7649271
visualstudio <visualstudio@122-PC28>
2019-04-12 18:35:53 -0400

    commit

--- c0725563-assign5/Program.cs
+++ c0725563-assign5/Program.cs
@@ -10,7 +10,7 @@ namespace c0725563_assignmnet5
     {
         string[] Letters = new string[26] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
 
-        public static void Main()
+        public static void Main() 
         {
 
             Program2 orange = new Program2();
